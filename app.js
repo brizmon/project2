051f835ac5b4b90c1272e2cdef019d5ab0f8cf42
Briseida Montiel <brizism@gmail.com>
2017-07-31 22:49:46 -0400

    dribbble api npm installed

--- app.js
+++ app.js
@@ -1,10 +1,33 @@
-const express = require('express')
-const app = express()
+// import express from our dependencies
+const express = require('express');
+const logger = require('morgan');
+const path = require('path');
+const bodyParser = require('body-parser');
+const dribbbleApi = require('dribbble-api')
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+// initialize the app
+const app = express();
 
-app.listen(3008, function () {
-  console.log('Example app listening on port 3008!')
-})
+//middlewares
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+
+// static files
+app.use(express.static('public'));
+
+// views
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// set the port, either from an environmental variable or manually
+const port = process.env.PORT || 3003;
+// tell the app to listen on that particular port
+app.listen(process.env.PORT || 3003, function () {
+  console.log('Example app listening on port ' + (process.env.PORT || 3003));
+});
+
+// Our index route
+app.get('/', (req, res) => {
+    res.send('Holaaa Mundo!!')
+});
